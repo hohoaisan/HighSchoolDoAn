ebbbe23594c31b65965e48e9c879d4672ec01c67
hoaisan <hohoaisan@gmail.com>
2021-08-13 15:48:23 +0700

    Decrease spacing in page content

--- src/styles/Dashboard/common.ts
+++ src/styles/Dashboard/common.ts
@@ -1,8 +1,8 @@
 import { useTheme, alpha, Theme, createStyles, makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme) => ({
   container: {
-    paddingTop: theme.spacing(4),
-    paddingBottom: theme.spacing(4),
+    paddingTop: theme.spacing(2),
+    paddingBottom: theme.spacing(2),
   },
   paper: {
     padding: theme.spacing(2),
